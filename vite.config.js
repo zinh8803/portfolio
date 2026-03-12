cd2680674b1360fb5d1955f4122a52373605cace
zinh8803 <ngoquocvinh2003@gmail.com>
2026-03-13 00:08:51 +0700

    done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Cần thiết khi deploy lên GitHub Pages
-  // Thay 'portfolio' bằng tên GitHub repository của bạn
-  base: '/portfolio/',
 })
