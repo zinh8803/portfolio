5b7f1617ab599bb01d080948ac534676d493af53
zinh8803 <ngoquocvinh2003@gmail.com>
2026-03-12 23:42:30 +0700

    chore: add github actions workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Cần thiết khi deploy lên GitHub Pages
+  // Thay 'portfolio' bằng tên GitHub repository của bạn
+  base: '/portfolio/',
 })
